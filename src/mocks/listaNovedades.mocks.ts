4b48c8a76502dab2cd9c548457f51eca7241845c
LeirbaG20 <102021254+LeirbaG20@users.noreply.github.com>
2022-05-04 14:15:44 +0200

    New

---
+++ src/mocks/listaNovedades.mocks.ts
@@ -0,0 +1,16 @@
+import { Track } from 'src/model/track';
+export const Mock_listNov: Track[] = [
+    {
+        title: 'Pop',
+        img: 'src/assets/img/pop-music.png'
+    },
+    {
+        title: 'Rock',
+        img: 'src/assets/img/rock-music.png'
+    },
+    {
+        title: 'House',
+        img: 'src/assets/img/house-music.png'
+    },
+
+]
---
+++ src/mocks/listaUrban.mocks.ts
@@ -0,0 +1,16 @@
+import { Track } from 'src/model/track';
+export const Mock_listUrb: Track[] = [
+    {
+        title: 'Rap',
+        img: 'src/assets/img/rap-music.png'
+    },
+    {
+        title: 'Reggaeton',
+        img: 'src/assets/img/reggaeton-music.png'
+    },
+    {
+        title: 'Trap',
+        img: 'src/assets/img/trap-music.png'
+    },
+
+]
