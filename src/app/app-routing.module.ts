fc519c1caf64a9ece2996f8d004beac031c71c4d
LeirbaG20 <102021254+LeirbaG20@users.noreply.github.com>
2022-05-15 05:13:53 +0200

    Ruta con links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageArtistCreateComponent } from './components/pages/page-artist-create/page-artist-create/page-artist-create.component';
 import { PageArtistComponent } from './components/pages/page-artist/page-artist/page-artist.component';
 import { PageHomeComponent } from './components/pages/page-home/page-home.component';
 
 const routes: Routes = [
   { path: '', component: PageHomeComponent},
   { path: 'artist', component: PageArtistComponent},
-  // { path: 'artist/create', component: },
+  { path: 'artist/create', component: PageArtistCreateComponent},
 ];
 
 @NgModule({
